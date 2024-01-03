39a269b0660336a9fde08873c3b34fe93c70d801
KAPIL THAKUR <KAPPU445@GMAIL.com>
2024-01-03 13:06:36 +0530

    Before and After Hooks in Testing

--- src/App.test.js
+++ src/App.test.js
@@ -48,7 +48,18 @@ describe("UI Based Testing", () => {
 // describe.only  ---- testing for only this case
 // describe.skip  ---- testing for skip this case
 
+// // Before hooks --
+
+// beforeAll(() => {
+//   console.log("*****Before All Hooks ****");
+// });
+
+// beforeEach(() => {
+//   console.log("*****Before Each Hooks ****");
+// });
+
 test("testing for onChange event", () => {
+  console.log("1st Test Case");
   render(<App />);
   let event = screen.getByRole("textbox");
   fireEvent.change(event, { target: { value: "kapil" } });
@@ -56,8 +67,19 @@ test("testing for onChange event", () => {
 });
 
 test("testing for onClick event", () => {
+  console.log("2nd Test Case");
   render(<App />);
   let btn = screen.getByRole("button");
   fireEvent.click(btn);
   expect(screen.getByText("updated data")).toBeInTheDocument();
 });
+
+// // After hooks --
+
+// AfterAll(() => {
+//   console.log("*****After All Hooks ****");
+// });
+
+// AfterEach(() => {
+//   console.log("*****After Each Hooks ****");
+// });
